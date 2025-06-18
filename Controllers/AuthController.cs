05c271439fa8841654bbcaeaf2cfeb5584c0bb7c
Khushali Kavathiya <khushalikavathiya@Solvatives-Air.lan>
2025-06-18 14:10:44 +0530

    add register and login method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using JwtRoleBased.Entities;
+using Microsoft.AspNetCore.Identity;
+using JwtRoleBased.Model;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
 namespace JwtRoleBased.Controllers
 {
-    
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private static User user = new();
+        private readonly IConfiguration configuration;
+         public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost("register")]
+
+        public ActionResult<User> Register(UserDto request)
+        {
+            user.Username = request.Username;
+            user.PasswordHash = new PasswordHasher<User>().HashPassword(user, request.Password);
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password");
+            }
+            string token = CreateToken(user);
+            return Ok(user);
+        }
+
+        private string CreateToken(User user)
+        {
+            var Claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: Claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+
 }
